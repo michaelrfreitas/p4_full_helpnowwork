e97716d3d85947d95f972edf13a1e7c1762c9412
Michael Freitas <michael_rfreitas@hotmail.com>
2022-09-11 16:52:26 +0000

    fix: try to fix the static files

--- helpnowwork/settings.py
+++ helpnowwork/settings.py
@@ -164,8 +164,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
