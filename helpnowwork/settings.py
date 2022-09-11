f75f779ad53060b3b77b1d132dea0c32669d24d7
Michael Freitas <michael_rfreitas@hotmail.com>
2022-09-11 13:03:30 +0000

    fix: try to static files work

--- helpnowwork/settings.py
+++ helpnowwork/settings.py
@@ -166,6 +166,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
