83239643159a5bab4639b67146c2bea67cf4a45e
Michael Freitas <michael_rfreitas@hotmail.com>
2022-09-12 20:12:32 +0000

    code: apply migrate rebuild sqlite3 db

--- sign_in/admin.py
+++ sign_in/admin.py
@@ -5,8 +5,7 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class UserAdmin (admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username',
-                    'email', 'profile', 'date_joined')
+    list_display = ('username', 'first_name', 'last_name', 'email', 'profile', 'date_joined')
     list_filter = ('profile', 'date_joined')
     search_fields = ('first_name', 'last_name', 'username',
                      'email', 'mobile', 'date_joined')
