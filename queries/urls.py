fbc6fcc2cb5e08ba652e7ec3899fc9681766afc8
Michael Freitas <michael_rfreitas@hotmail.com>
2022-09-19 23:04:05 +0000

    code: answer function done with crud

--- queries/urls.py
+++ queries/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     path('send_question', views.send_question, name='send_question'),
-    path('send_answer', views.send_answer, name='send_answer'),
+    path('send_answer/<question_slug>', views.send_answer, name='send_answer'),
     path('question_list', views.question_list, name='question_list'),
     path('edit/<question_slug>', views.edit_question, name='edi_question'),
     path('delete/<question_slug>', views.delete_question, name='delete_question'),
     path('all_questions', views.all_questions, name='all_questions'),
-    path('view_question/<question_slug>', views.view_question, name='view_question')
+    path('view_question/<question_slug>', views.view_question, name='view_question'),
+    path('answer_list', views.answer_list, name='answer_list'),
+    path('edit_answer/<answer_id>', views.edit_answer, name='edit_answer'),
+    path('delete_answer/<answer_id>', views.delete_answer, name='delete_answer'),
+    path('approved_answer/<answer_id>', views.approved_answer, name='approved_answer')
 ]
--- queries/views.py
+++ queries/views.py
@@ -29,18 +29,22 @@ def send_question(request):
 
 
 @login_required(redirect_field_name='account_login')
-def send_answer(request):
+def send_answer(request, question_slug):
     if request.user.profile == 'CONTRIBUTOR':
+        question = get_object_or_404(QuestionModel, slug=question_slug)
         if request.method == 'POST':
             form = AnswerForm(request.POST)
             if form.is_valid():
                 element = form.save(commit=False)
+                element.subject_question = QuestionModel.objects.get(
+                    slug=question_slug)
                 element.contributor = request.user
                 element.save()
-                return redirect('answer_list')
+                return redirect('all_questions')
         form = AnswerForm()
         context = {
-            'form': form
+            'form': form,
+            'question': question
         }
         return render(request, 'queries/send_answer.html', context)
     return render(request, 'portal/error.html')
@@ -59,6 +63,7 @@ def question_list(request):
 def edit_question(request, question_slug):
     """This is a docstring which describes the module"""
     question = get_object_or_404(QuestionModel, slug=question_slug)
+    answer = AnswerModel.objects.filter(subject_question=QuestionModel.objects.get(slug=question_slug), approved=True)
     if request.method == 'POST':
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
@@ -68,7 +73,8 @@ def edit_question(request, question_slug):
             messages.warning(request, 'This question already exists.')
     form = QuestionForm(instance=question)
     context = {
-        'form': form
+        'form': form,
+        'answer': answer
     }
     return render(request, 'queries/edit_question.html', context)
 
@@ -93,7 +99,60 @@ def all_questions(request):
 @login_required(redirect_field_name='account_login')
 def view_question(request, question_slug):
     question = QuestionModel.objects.filter(slug=question_slug)
+    answer = AnswerModel.objects.filter(
+        subject_question=QuestionModel.objects.get(slug=question_slug), approved=True)
     context = {
-        'question': question
+        'question': question,
+        'answer': answer
     }
     return render(request, 'queries/view_question.html', context)
+
+
+@login_required(redirect_field_name='account_login')
+def answer_list(request):
+    if request.user.profile == 'ADMINISTRATOR':
+        answers = AnswerModel.objects.all()
+    else:
+        answers = AnswerModel.objects.filter(contributor=request.user)
+    context = {
+        'answers': answers
+    }
+    return render(request, 'queries/answer_list.html', context)
+
+
+@login_required(redirect_field_name='account_login')
+def edit_answer(request, answer_id):
+    """This is a docstring which describes the module"""
+    answer = get_object_or_404(AnswerModel, id=answer_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            element = form.save(commit=False)
+            element.approved = False
+            element.save()
+            return redirect('answer_list')
+        else:
+            messages.warning(request, 'This answer already exists.')
+    form = AnswerForm(instance=answer)
+    context = {
+        'form': form,
+        'answer': answer
+
+    }
+    return render(request, 'queries/edit_answer.html', context)
+
+
+@login_required(redirect_field_name='account_login')
+def delete_answer(request, answer_id):
+    """This is a docstring which describes the module"""
+    answer = get_object_or_404(AnswerModel, id=answer_id)
+    answer.delete()
+    return redirect('answer_list')
+
+@login_required(redirect_field_name='account_login')
+def approved_answer(request, answer_id):
+    """This is a docstring which describes the module"""
+    answer = get_object_or_404(AnswerModel, id=answer_id)
+    answer.approved = not answer.approved
+    answer.save()
+    return redirect('answer_list')
